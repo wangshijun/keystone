fbaede0de50b9086b5c2e60785459e6da375a594
howell <hongjiawei@weego.me>
2017-12-12 15:02:32 +0800

    expand nodelete, noadd option of ListField

--- fields/types/Type.js
+++ fields/types/Type.js
@@ -115,9 +115,6 @@ Field.prototype.getOptions = function () {
 			optionKeys = optionKeys.concat(this._properties);
 		}
 		optionKeys.forEach(function (key) {
-			if (key === 'noadd') {
-				console.log('================', this[key]);
-			}
 			if (this[key]) {
 				this.__options[key] = this[key];
 			} else if (this.options[key]) {
